408e8882222ac03c25668816c3f1a4c6fe97419e
Adeel Bhatt <adeelbhatti88@gmail.com>
2020-08-15 19:42:53 -0500

    variable file commit

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+let name = 'sephiroth' //use let for variables, javascript knows this is a string
+let score = 100
+let bonus = 20
+
+let totalScore = score + bonus
+
+console.log("Hello", name, "!", "your score is ", totalScore); //you don't need semicolon after the bracket, javascript is easy going with that stuff.
+
+let firstName = 'John'
+let lastName = 'Doe'
+
+let fullName = firstName + ' ' + lastName
+
+console.log(fullName)
