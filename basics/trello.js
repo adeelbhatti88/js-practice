4f22b244e69af57e6b9f04fce0fa17985dee37f5
Adeel Bhatt <adeelbhatti88@gmail.com>
2020-08-21 22:57:01 -0500

    for each loop

---
+++ basics/trello.js
@@ -0,0 +1,11 @@
+const days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat']
+
+console.log(days[0]);
+
+
+//below we are putting a function inside a forEach loop, this function will execute based on how many elements you have in the array.
+days.forEach(function (day, index) {
+
+    console.log(`starts with ${index + 1}  -- ${day}`);
+    
+})
