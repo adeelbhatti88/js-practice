bc4f9939fe6271618bb7bb0f8708b8186aabec06
Adeel Bhatt <adeelbhatti88@gmail.com>
2020-08-27 20:06:30 -0500

    == and === example

---
+++ basics/equals.js
@@ -0,0 +1,5 @@
+let myVar = {}
+let myVarTwo = {}
+
+console.log(myVar === myVarTwo); //this will be false, because the 2 variables are refrencing two different objects.
+//to make the above statement print true, you would have to set myVarTwo = myVar
