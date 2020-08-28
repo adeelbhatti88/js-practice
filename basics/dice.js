b9574daf91fd33d666c1b935819585c526041439
Adeel Bhatt <adeelbhatti88@gmail.com>
2020-08-27 19:59:04 -0500

    simple random number generator.

---
+++ basics/dice.js
@@ -0,0 +1,19 @@
+// let numOne = 22
+// let numTwo = 7
+
+// result = 22/7
+
+// console.log(result.toFixed(2));
+// let floatRes = result.toFixed(2)
+
+// console.log(Math.floor(floatRes));
+// console.log(Math.ceil(floatRes));
+
+// console.log(Math.random() * 10); //you can multiply the random function with the value you want the random number to generate up to.
+
+let upper = 6
+let lower = 1
+
+let myRandom = Math.floor(Math.random() * (upper - lower + 1)) + lower //this generates a random number between 20 and 25.
+
+console.log(myRandom);
